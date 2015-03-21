247b4961cabe678875ab6ba68c0d5d296daa688a
Alex Naumchenko <alex@devloft.com>
2015-03-21 19:41:02 -0400

    Empty cookbook.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'kitchen-foreman'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures kitchen-foreman'
+long_description 'Installs/Configures kitchen-foreman'
+version          '0.1.0'
